ccebf0f806d141c167ee9f540ad9dd435c058995
zubi <zubayerahmed232@gmail.com>
2020-10-30 18:44:15 +0600

    build deleted

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-import Avatar from "@material-ui/core/Avatar";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
-import Typed from "react-typed";
 import { makeStyles } from "@material-ui/core/styles";
 import myself from "../images/sakib2.png";
 
